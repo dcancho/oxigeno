21224923897a047f874b07ce025f8e60caf84041
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2024-05-20 07:32:35 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Hello World!' });
+});
+
+app.get('/oxygen-providers', (req, res) => {
+	const patientId = req.query.patientId;
+	const healthcareCenter = req.query.healthcareCenter;
+	const oxygenRequirement = req.query.oxygenRequirement;
+	const searchIntensity = req.query.searchIntensity;
+	res.json({ 
+		message: `Oxygen providers list for patientId: ${patientId} and healthcareCenter: ${healthcareCenter} and oxygenRequirement: ${oxygenRequirement} and searchIntensity: ${searchIntensity}`
+	});
+})
+
+app.post('/oxygen-request', (req, res) => {
+    const patientId = req.body.patientId;
+    const healthcareCenter = req.body.healthcareCenter;
+    const oxygenRequirement = req.body.oxygenRequirement;
+    const patientDiagnosis = req.body.patientDiagnosis;
+
+    res.json({ 
+        message: `Oxygen request received! with patientId: ${patientId} and healthcareCenter: ${healthcareCenter} and oxygenRequirement: ${oxygenRequirement} and patientDiagnosis: ${patientDiagnosis}`
+    });
+});
+
+app.listen(3000, () => {
+	  console.log('Example app listening on port 3000!');
+});
