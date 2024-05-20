63f0d238a782111ae2e5a8a7e0679ff84fc82aee
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2024-05-20 11:39:11 -0500

    code: add mongodb driver and modify controller

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
-const express = require('express');
+import express, { json } from "express";
+import { MongoClient } from "mongodb";
+
 const app = express();
 
-app.use(express.json());
+app.use(json());
 
-app.get('/test', (req, res) => {
-	res.json({ message: 'Hello World from Express!' });
+app.get("/test", (req, res) => {
+  res.json({ message: "Hello World from Express!" });
 });
 
-app.get('/oxygen-providers', (req, res) => {
-	const patientId = req.query.patientId;
-	const healthcareCenter = req.query.healthcareCenter;
-	const oxygenRequirement = req.query.oxygenRequirement;
-	const searchIntensity = req.query.searchIntensity;
-	res.json({ 
-		message: `Oxygen providers list for patientId: ${patientId} and healthcareCenter: ${healthcareCenter} and oxygenRequirement: ${oxygenRequirement} and searchIntensity: ${searchIntensity}`
-	});
-})
+app.get("/oxygen-providers", async (req, res) => {
+  const queryPatientId = req.query.patientId;
+  const queryHealthcareCenter = req.query.healthcareCenter;
+  const queryOxygenRequirement = req.query.oxygenRequirement;
+  const searchIntensity = req.query.searchIntensity;
 
-app.post('/oxygen-request', (req, res) => {
-    const patientId = req.body.patientId;
-    const healthcareCenter = req.body.healthcareCenter;
-    const oxygenRequirement = req.body.oxygenRequirement;
-    const patientDiagnosis = req.body.patientDiagnosis;
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    console.error("Mongo URI is not set");
+    return res.status(500).json({ message: "Internal server error" });
+  }
 
-    res.json({ 
-        message: `Oxygen request received! with patientId: ${patientId} and healthcareCenter: ${healthcareCenter} and oxygenRequirement: ${oxygenRequirement} and patientDiagnosis: ${patientDiagnosis}`
-    });
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const oxygenRequests = client
+      .db("dev_oxigeno")
+      .collection("oxygenRequests");
+    const result = await oxygenRequests.findOne({ patientId: queryPatientId });
+    if (result) {
+      res.json({ message: "Oxygen request already exists for this patient" });
+    } else {
+      res.json({
+        message: `Oxygen providers list for patientId: ${queryPatientId} and healthcareCenter: ${queryHealthcareCenter} and oxygenRequirement: ${queryOxygenRequirement} and searchIntensity: ${searchIntensity}`,
+      });
+    }
+  } catch (err) {
+	console.error(err);
+    res
+      .status(500)
+      .json({ message: "An error occurred." });
+  } finally {
+    await client.close();
+  }
+});
+
+app.post("/oxygen-request", (req, res) => {
+  const patientId = req.body.patientId;
+  const healthcareCenter = req.body.healthcareCenter;
+  const oxygenRequirement = req.body.oxygenRequirement;
+  const patientDiagnosis = req.body.patientDiagnosis;
+
+  res.json({
+    message: `Oxygen request received! with patientId: ${patientId} and healthcareCenter: ${healthcareCenter} and oxygenRequirement: ${oxygenRequirement} and patientDiagnosis: ${patientDiagnosis}`,
+  });
 });
 
 const port = process.env.PORT || 8080;
